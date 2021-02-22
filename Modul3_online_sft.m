4a7a6aa398e18fb7f46feb9b52b608ba9955273b
dzakyraffy <dzakyale-dzakly@yahoo.co.id>
2021-02-23 02:16:52 +0700

    disini hanya markah dari final
    ya lumayan lah untuk markah
    selebihnya kode ini gw anggap stabil
    selamat menggunakan dan jangan lupa manfaatkan sebaik mungkin
    
    . thanks a lot untuk Evan dan Iqbal untuk bantuannya

--- Modul3_online_sft.m
+++ Modul3_online_sft.m
@@ -1,11 +1,11 @@
 %unfinished block for Eplane%langsung gerak ke penugasan
-%using spec B
+%using spec B >>l=lambda*2.1:0.1:lambda*3
 c=3e8;%agar bisa pakai Hz
 f=3e9;%frekuensi kerjadalam Hz
 lambda=c/f;%menentukan panjang gelombang
 eta=377;%impedansi ruang bebas
 I=1;%arus maksimum masuk antena
-l=lambda/2;%panjang antena. bisa berubah
+l=lambda*1.5;%panjang antena. bisa berubah
 k=(2*pi)/lambda;%wavenuber
 theta=(0:0.01:2*pi);%sudut elevasi
 r=10*lambda;%radius spherical coordinate/ bisa dibilang magnitude.
@@ -19,9 +19,10 @@ Eplane=part1.*part2;
 %unfinished block for Eplane%
 
 %unfinished array factor%
-N=16;%jumlah array antena
+%spec B beta=pi d=lambda/2 and n=10,30,50
+N=50;%jumlah array antena
 d=lambda/2;%jarak antar elemen array/spacingnya lambda/2
-beta=0;%fase eksitasi
+beta=1*pi;%fase eksitasi safe-bet 1*pi
 psi=k*d*cos(theta)+beta;
 AF=(1/N)*((sin((N/2)*psi))./sin((1/2)*psi));
 %unfinished array factor%
@@ -30,7 +31,8 @@ AF=(1/N)*((sin((N/2)*psi))./sin((1/2)*psi));
 Etotal=Eplane.*AF;
 
 %Power pattern dan konversi magnitude ke dB
-rtheta=abs(Eplane);%bisa Eplane atau AF
+%rtheta sudah dikondisikan untuk entry plot 3D
+rtheta=abs(Etotal);%bisa Eplane atau AF atau Etotal
 Rtheta=10*log10(rtheta);
 
 %Rnormalisasi
@@ -61,5 +63,9 @@ z=Rnorm_matrix .*sin(theta_matrix);
 
 %plotting
 %polarplot(Rtheta); ini sudah siap
+figure(1);
 polarplot(10*log10(abs(Eplane.*AF)));%tidak digabung konversinya ke dB dulu
-%surf(x,y,z,sqrt(x.^2+y.^2+z.^2),'EdgeColor','none','FaceColor','interp');
+figure(2);
+surf(x,y,z,sqrt(x.^2+y.^2+z.^2),'EdgeColor','none','FaceColor','interp');
+figure(3);
+polarplot(10*log10(abs(AF)));
